52916291ca5faa3024f45019bec7055bb8c131ca
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-08-19 18:38:35 +0100

    REMOTE SETUP

---
+++ src/main/java/com/example/TillRecords/TillRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TillRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TillRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TillRecordsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/TillRecords/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.example.TillRecords.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "YOU ARE AT THE HOMEPAGE";
+    }
+
+}
