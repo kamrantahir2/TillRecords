c615a5b5dafd65ab17b658806a6ec9732c4e5c90
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-08-19 19:50:25 +0100

    Create Till model

--- src/main/java/com/example/TillRecords/controller/WebController.java
+++ src/main/java/com/example/TillRecords/controller/WebController.java
@@ -1,5 +1,8 @@
 package com.example.TillRecords.controller;
 
+import com.example.TillRecords.model.Till;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @GetMapping("/")
-    public String home() {
-        return "YOU ARE AT THE HOMEPAGE";
+    public String home() throws JsonProcessingException {
+        Till till = new Till("1", 2, 3, 4);
+
+        return objectMapper.writeValueAsString(till);
     }
 
 }
---
+++ src/main/java/com/example/TillRecords/model/Till.java
@@ -0,0 +1,92 @@
+package com.example.TillRecords.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Till {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String dateTime;
+    private String tillName;
+    private double coinsAndFivePounds;
+    private double remainingNotes;
+    private double overUnder;
+
+//    Methods
+
+    public void print() {
+        System.out.println("date: " + this.dateTime);
+        System.out.println("tillName: " + this.tillName);
+        System.out.println("coinsAndFivePounds: " + this.coinsAndFivePounds);
+        System.out.println("remainingNotes: " + this.remainingNotes);
+        System.out.println("overUnder: " + this.overUnder);
+    }
+
+
+
+//    Constructors
+
+    public Till(String tillName, double coinsAndFivePounds, double remainingNotes, double overUnder) {
+        this.tillName = tillName;
+        this.coinsAndFivePounds = coinsAndFivePounds;
+        this.remainingNotes = remainingNotes;
+        this.overUnder = overUnder;
+        LocalDateTime localDate = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        this.dateTime = localDate.format(dateTimeFormatter);
+    }
+
+    public Till() {
+    }
+
+    //  GETTERS AND SETTERS
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getTillName() {
+        return tillName;
+    }
+
+    public void setTillName(String tillName) {
+        this.tillName = tillName;
+    }
+
+    public double getCoinsAndFivePounds() {
+        return coinsAndFivePounds;
+    }
+
+    public void setCoinsAndFivePounds(double coinsAndFivePounds) {
+        this.coinsAndFivePounds = coinsAndFivePounds;
+    }
+
+    public double getRemainingNotes() {
+        return remainingNotes;
+    }
+
+    public void setRemainingNotes(double remainingNotes) {
+        this.remainingNotes = remainingNotes;
+    }
+
+    public double getOverUnder() {
+        return overUnder;
+    }
+
+    public void setOverUnder(double overUnder) {
+        this.overUnder = overUnder;
+    }
+}
